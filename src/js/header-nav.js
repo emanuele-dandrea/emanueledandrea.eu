05b51e0c517b34605cfbf783dbd37da47f304130
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-02-12 15:25:22 +0100

    JavaScript remove

--- src/js/header-nav.js
+++
@@ -1,6 +0,0 @@
-const header = document.querySelector(".header");
-const toggle = document.querySelector(".menu-toggle");
-
-toggle.addEventListener("click", () => {
-  header.classList.toggle("menu-open");
-});
